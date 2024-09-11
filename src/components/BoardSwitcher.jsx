9d228870f9a8136ce30d72d7d689c154c915c1f1
Kevin Chen <kevin.chen779@qmail.cuny.edu>
2024-09-11 19:49:37 -0400

    Week03 Inclass project1

--- src/components/BoardSwitcher.jsx
+++ src/components/BoardSwitcher.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Board(props) {
   let className = "board";
@@ -10,16 +10,22 @@ function Board(props) {
 }
 
 function BoardSwitcher(props) {
+  const [board, setBoard] = useState(0);
+  
   let boards = [];
   for (let ii = 0; ii < props.numBoards; ii++) {
-    let isSelected = ii === 0;
+    let isSelected = ii === board;
     boards.push(<Board index={ii} selected={isSelected} key={ii} />);
+  }
+
+  function Toggle() {
+    setBoard((board + 1) % 5);
   }
 
   return (
     <div>
       <div className="boards">{boards}</div>
-      <button>Toggle</button>
+      <button onClick = {Toggle}>Toggle</button>
     </div>
   );
 }
