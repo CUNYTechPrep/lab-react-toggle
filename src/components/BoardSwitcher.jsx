7bd1fa3462ee55484f525bb0dba89285559d2e84
Sania Pervez <139937275+sanipervez@users.noreply.github.com>
2024-09-11 20:20:00 -0400

    Lab completed

--- src/components/BoardSwitcher.jsx
+++ src/components/BoardSwitcher.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Board(props) {
   let className = "board";
@@ -10,18 +10,23 @@ function Board(props) {
 }
 
 function BoardSwitcher(props) {
+  const [selectedBoard, setSelectedBoard] = useState(0);
+
+  const toggleBoard = () => {
+    setSelectedBoard((prevBoard) => (prevBoard + 1) % props.numBoards); 
+  };
+
   let boards = [];
   for (let ii = 0; ii < props.numBoards; ii++) {
-    let isSelected = ii === 0;
+    let isSelected = ii === selectedBoard;
     boards.push(<Board index={ii} selected={isSelected} key={ii} />);
   }
 
   return (
     <div>
       <div className="boards">{boards}</div>
-      <button>Toggle</button>
+      <button onClick={toggleBoard}>Toggle</button>
     </div>
   );
 }
-
 export default BoardSwitcher;
--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,12 @@ import BoardSwitcher from "./components/BoardSwitcher";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//switch from 3 -> 5 boards
+//BoardSwitcher allows for toggle button. if you give it a specific amount it 
+//will create the boxes
 root.render(
   <React.StrictMode>
-    <BoardSwitcher numBoards={3} />
+    <BoardSwitcher numBoards={5} />
   </React.StrictMode>
 );
