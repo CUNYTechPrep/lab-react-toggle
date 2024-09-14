d4cb363a9bfe1b5d4ce774e509f771f125b55423
jpntc <judepierre4477@gmail.com>
2024-09-14 16:32:33 -0400

    implemented class solution

--- src/components/BoardSwitcher.jsx
+++ src/components/BoardSwitcher.jsx
@@ -10,17 +10,28 @@ function Board(props) {
 }
 
 function BoardSwitcher(props) {
-  const [selected, setSelected] = useState(1);
-  let boards = [];
-  for (let ii = 0; ii < props.numBoards; ii++) {
-   let isSelected = selected === ii + 1
-    boards.push(<Board index={ii} selected={isSelected} key={ii} />);
+  // const [selected, setSelected] = useState(1);
+  const [selectedBoardIndex, setSelectedBoardIndex] = useState(0);
+
+  function handleClick (){
+    setSelectedBoardIndex( (selectedBoardIndex + 1) % props.numBoards )
+    
   }
 
+  let boards = [];
+  // for (let ii = 0; ii < props.numBoards; ii++) {
+  //  let isSelected = selected === ii + 1
+  //   boards.push(<Board index={ii} selected={isSelected} key={ii} />);
+    
+  for (let boardIndex = 0; boardIndex < props.numBoards; boardIndex++) {
+    let isBoardSelected = selectedBoardIndex === boardIndex;
+    boards.push(<Board index={boardIndex} selected = {isBoardSelected} key={boardIndex}/>)
+  }
   return (
     <div>
       <div className="boards">{boards}</div>
-      <button onClick={()=>{setSelected(selected === boards.length? 1 : selected + 1); console.log(selected)}}>Toggle</button>
+      {/* <button onClick={()=>{setSelected(selected === boards.length? 1 : selected + 1); console.log(selected)}}>Toggle</button> */}
+      <button onClick={handleClick}>Toggle</button> 
     </div>
   );
 }
