fde3fd799570fee927493d0ba0359824499136c5
Zakaria Chowdhury <zc@zakarias-air.mynetworksettings.com>
2024-09-20 17:56:56 -0400

    Assignment Submission

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import BoardSwitcher from "./components/BoardSwitcher";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <BoardSwitcher numBoards={3} />
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
